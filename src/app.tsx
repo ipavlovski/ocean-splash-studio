d600380d911cb3d8d9181d1ca4bd6ea0a0c1fc9f
IP <ilya.pavlovski@gmail.com>
2023-10-04 14:00:17 -0300

    Under construction rough-in.

--- src/app.tsx
+++ src/app.tsx
@@ -1,10 +1,46 @@
 import { css } from 'styled-system/css'
+import { Center, Flex } from 'styled-system/jsx'
+
+import ReactLogo from '../assets/splash.svg?react'
+
+function Splash() {
+  const styles = css({
+    width: '50vw',
+    height: '50vh',
+  })
+
+  return (
+    <div className={styles}>
+      <img src='./assets/ocean-splash-low-poly-2.png' />
+    </div>
+  )
+}
+
+function ConstructionText() {
+  const styles = css({
+    // textTransform: 'uppercase',
+    fontSize: '4rem',
+    fontWeight: 'bold',
+    color: 'hsl(211, 56%, 18%)',
+    fontFamily: 'pacifico',
+    letterSpacing: 'widest'
+  })
+  return (
+    <h1 className={styles}>
+      under construction
+    </h1>
+  )
+}
 
 export default function App() {
   const styles = css({
-    background: 'slate.500',
-    color: 'slate.100'
+    backgroundImage: 'linear-gradient(to top, #fff1eb 0%, #ace0f9 100%)',
   })
 
-  return <h1 className={styles}>Ocean Splash</h1>
+  return (
+    <Flex width='100vw' height='100vh' flexDir='column' className={styles} alignItems='center'>
+      <ReactLogo />
+      <ConstructionText />
+    </Flex>
+  )
 }
--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,37 @@
 import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
 import FullReload from 'vite-plugin-full-reload'
+import svgr from 'vite-plugin-svgr'
 
 export default ({ mode }: { mode: string }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) }
 
   return defineConfig({
-    plugins: [react()], // FullReload(['src/app.tsx'])
+    plugins: [
+      react(),
+      svgr({}),
+    ], // FullReload(['src/app.tsx'])
 
     server: {
       port: parseInt(process.env.VITE_PORT!),
       host: true,
       https: {
         key: `${process.env.HOME}/.config/ssl/local/local.key`,
-        cert: `${process.env.HOME}/.config/ssl/local/local.crt`
-      }
+        cert: `${process.env.HOME}/.config/ssl/local/local.crt`,
+      },
     },
 
     resolve: {
       alias: [
         {
           find: 'src',
-          replacement: `${__dirname}/src`
+          replacement: `${__dirname}/src`,
         },
         {
           find: 'styled-system',
-          replacement: `${__dirname}/styled-system`
-        }
-      ]
-    }
+          replacement: `${__dirname}/styled-system`,
+        },
+      ],
+    },
   })
 }
